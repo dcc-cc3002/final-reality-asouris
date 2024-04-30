4d62bf1286bf0ff028939c898fe2d32dfed9b54d
juju <a-github@asouris.com>
2024-04-29 21:22:34 -0400

    ✅ General character and enemy testing

--- src/test/scala/character/CharacterTest.scala
+++ src/test/scala/character/CharacterTest.scala
@@ -1,5 +1,14 @@
 package character
+import character.players.Warrior
 
-class CharacterTest {
+class CharacterTest extends munit.FunSuite{
 
+
+  test("isDefeated should return true if the life od a character has reached 0, false otherwise") {
+    var enemy = Enemy("enemy", 10, 5, 30, 20)
+    var warrior = Warrior("warrior", 0, 20, 50)
+
+    assert(warrior.isDefeated)
+    assert(!enemy.isDefeated)
+  }
 }
--- src/test/scala/character/EnemyTest.scala
+++ src/test/scala/character/EnemyTest.scala
@@ -1,26 +1,16 @@
 package character
 
 import character.Enemy
+import weapon.Sword
 
 class EnemyTest extends munit.FunSuite {
 
-  test("An Enemy should be defeated when its life points reach zero") {
-    val enemy = new Enemy("Goblin", 10, 5, 2, 50) // Creating an enemy with 10 life points
-    assert(!enemy.isDefeated) // Enemy should not be defeated initially
+  test("equals"){
+    var enemy0 = Enemy("enemy0", 10, 20, 30, 40)
+    var enemy1 = Enemy("enemy1", 10, 15, 30, 2)
 
-    // Inflict damage until the enemy's life points reach zero
-    (1 to 10).foreach(_ => enemy.life -= 1)
-
-    assert(enemy.isDefeated) // Enemy should be defeated when life points reach zero
-  }
-
-  test("An Enemy should not be defeated when its life points are positive") {
-    val enemy = new Enemy("Orc", 20, 8, 3, 80) // Creating an enemy with 20 life points
-    assert(!enemy.isDefeated) // Enemy should not be defeated initially
-
-    // Inflict damage but ensure life points remain positive
-    (1 to 10).foreach(_ => enemy.life -= 1)
-
-    assert(!enemy.isDefeated) // Enemy should not be defeated when life points are positive
+    assertEquals(enemy1, enemy1)
+    assertEquals(enemy1, new Enemy("enemy1", 10, 15, 30, 2))
+    assert(!enemy1.equals(enemy0))
   }
 }
