67f9aa18897879261487518dad4d67cf1d185cd7
juju <a-github@asouris.com>
2024-04-30 16:35:53 -0400

    🩹 Fixed exceptions

--- src/main/scala/character/players/Player.scala
+++ src/main/scala/character/players/Player.scala
@@ -1,12 +1,13 @@
 package character.players
 import character.Character
+import exceptions.WeaponException
 import weapon.Weapon
 
 trait Player extends Character{
   var equippedWeapon : Option[Weapon]
 
   def equip(weapon: Weapon): Unit = {
-    //exeption
+    throw WeaponException(s"$this cannot equip $weapon")
   }
 
 }
