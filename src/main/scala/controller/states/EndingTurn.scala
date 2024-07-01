710c3e3acbb33eb6092b55ed6031a2d50abe368f
juju <a-github@asouris.com>
2024-07-01 17:58:24 -0400

    :art: Added Ending State

--- src/main/scala/controller/states/EndingTurn.scala
+++ src/main/scala/controller/states/EndingTurn.scala
@@ -1,5 +1,18 @@
 package controller.states
+import controller.GameController
 
 class EndingTurn extends GameState{
+  //if we got here that means that the game hasn't finished
+  //we only need to figure if we need more turns or not
+  override def update(controller: GameController): Unit = {
+    
+    //if there is at least a turn 
+    if(controller.areTurns()){
+      controller.setState(new BeginningTurn)
+    }
+    else{
+      controller.setState(new SettingTurns)
+    }
+  }
 
 }
