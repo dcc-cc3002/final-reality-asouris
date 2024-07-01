a3dd6255d8ee50b83fa95ed6543054014903562d
juju <a-github@asouris.com>
2024-07-01 17:47:17 -0400

    :art: Compiled different outcomes in to EndingTurn

--- src/main/scala/controller/states/BeginningTurn.scala
+++ src/main/scala/controller/states/BeginningTurn.scala
@@ -4,20 +4,25 @@ import controller.GameController
 class BeginningTurn extends GameState{
 
   override def update(controller: GameController): Unit = {
+    //gets next character
     val character = controller.nextTurn()
+    //reset its action bar back to 0
+    controller.resetActionBar(character)
     
+    //checks for effects
     if(character.hasEffects){
       character.applyEffects()
 
-
-      if(character.isDefeated && !character.){
-        controller.handlingNewTurn()
+      //if character died or is paralyzed
+      //loses their turn
+      if(character.isDefeated || character.isParalyzed) {
+        controller.setState(new EndingTurn)
       }
-      else if(!character.isParalyzed){
+      else{//character just got healing we go forward
         controller.currentTurn = Some(character)
         controller.setState(new ChoosingWeapon())
       }
-    }
+    }//if no effects we go forward
     else{
       controller.currentTurn = Some(character)
       controller.setState(new ChoosingWeapon())
