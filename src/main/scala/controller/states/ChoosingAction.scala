2600d8c6c8f6b44e3f043df64f148dc2d59ec879
juju <a-github@asouris.com>
2024-07-01 17:48:43 -0400

    :sparkles: Added states

--- src/main/scala/controller/states/ChoosingAction.scala
+++ src/main/scala/controller/states/ChoosingAction.scala
@@ -1,5 +1,32 @@
 package controller.states
+import controller.GameController
+import exceptions.NoneException
+
+import scala.io.StdIn
 
 class ChoosingAction extends GameState{
+
+  //some player choose a weapon a now we need to decide on attack or spell
+  //only magical players can do a spell
+  override def update(controller: GameController): Unit = {
+    
+    var actions : Array[GameState] = null
+
+    controller.currentTurn match {
+      case Some(character) => actions = character.getActions
+      case None => throw NoneException("Something went wrong")
+    }
+    //map states to number for choosing :)
+    if(actions.length > 1){
+      print("You can take one of the following actions\nAttacking an enemy (1)\nCasting a spell (2)")
+      val selected = StdIn.readInt()
+      controller.setState(actions(selected-1))
+    }
+    else{
+      print("You must attack an enemy")
+      controller.setState(new AttackState)
+    }
+
+  }
 
 }
--- src/main/scala/controller/states/ChoosingWeapon.scala
+++ src/main/scala/controller/states/ChoosingWeapon.scala
@@ -1,5 +1,52 @@
 package controller.states
+import controller.GameController
+import character.Character
+import exceptions.NoneException
+
+import scala.io.StdIn
 
 class ChoosingWeapon extends GameState{
+
+  override def update(controller: GameController): Unit = {
+    //if we got here, we have a character for playing in controller.currentTurn
+    var character : Character = null
+    controller.currentTurn match {
+      case Some(value) => character = value
+      case None => throw NoneException("Something went wrong")
+    }
+    
+    //we try to get a weapon set
+    controller.getWeapons.get(character) match {
+      //if got, then is player
+      case Some(array) =>
+        //select option
+        print(character.toString  + " has 3 weapons available:")
+        var again = -1
+        var selected = -1
+        //loop to keep on changing weapon
+        while(again != 0){
+          //present options
+          var i = 1
+          array.foreach(weapon => {
+            print("(" + i + ") " + weapon)
+          })
+          print("Select your choice (1, 2, 3)")
+          selected = StdIn.readInt()
+          
+          //ask to continue
+          print("Would you like to choose again? (0 or 1)")
+          again = StdIn.readInt()
+        }
+        //equip choice. Keep in mind the array only contains eligible weapons for that character.
+        character.equip(array(selected-1))
+        
+        //next state
+        controller.setState(new ChoosingAction)
+        
+      //if nothing, character is enemy
+      case None =>
+        controller.setState(new AttackState)
+    }
+  }
 
 }
