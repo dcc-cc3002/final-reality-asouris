6640759c45aa922c6742e4cc1eb32ca9d8f727d9
juju <a-github@asouris.com>
2024-07-01 17:51:48 -0400

    :bug: Added weapons input for characters to choose

--- src/main/scala/controller/GameBattle.scala
+++ src/main/scala/controller/GameBattle.scala
@@ -2,17 +2,21 @@ package controller
 
 import character.Enemy
 import party.Party
+import character.Character
+import weapon.Weapon
 
+import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
 
-class GameBattle (private val party : Party, private val enemies: ArrayBuffer[Enemy]){
+class GameBattle (private val _party : Party, private val _enemies: ArrayBuffer[Character], private val _weapons : mutable.Map[Character, ArrayBuffer[Weapon]]){
   
   def run() : Unit = {
-    val controller = new GameController(party, enemies)
+    val controller = new GameController(_party, _enemies, _weapons)
     while(!controller.checkFinished()) {
       controller.update()
     }
-    
+
+    controller.displayEndMessage()
   }
   
   
